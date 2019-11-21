82f468c395fc80951011f1f7cc41db8b420d13ca
Ha Le <ha.le1@mohawkcollege.ca>
2019-11-21 02:15:42 -0500

    Update WelcomeFragment.java

--- Capstone/Capstone/app/src/main/java/ca/mohawk/le/mytime/WelcomeFragment.java
+++ Capstone/Capstone/app/src/main/java/ca/mohawk/le/mytime/WelcomeFragment.java
@@ -3,13 +3,20 @@ package ca.mohawk.le.mytime;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 
 /**
@@ -17,7 +24,7 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 public class WelcomeFragment extends Fragment {
     private View view;
-
+    private static final String TAG = "MainActivity";
     public WelcomeFragment() {
         // Required empty public constructor
     }
@@ -30,6 +37,25 @@ public class WelcomeFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_welcome, container, false);
 
         FirebaseDatabase.getInstance().getReference().keepSynced(true);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "getInstanceId failed", task.getException());
+                    return;
+                }
+
+                // Get new Instance ID token
+                String token = task.getResult().getToken();
+
+                // Log and toast
+                //String msg = getString(R.string.msg_token_fmt, token);
+                Log.d(TAG, token);
+                //Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return view;
     }
