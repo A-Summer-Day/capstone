b1b80985a8dda8d31f3846ff207f1f61fafc3a75
Ha Le <ha.le1@mohawkcollege.ca>
2019-11-24 05:33:17 -0500

    updated

--- Capstone/Capstone/app/src/main/java/ca/mohawk/le/mytime/ReportsFragment.java
+++ Capstone/Capstone/app/src/main/java/ca/mohawk/le/mytime/ReportsFragment.java
@@ -4,18 +4,23 @@ package ca.mohawk.le.mytime;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ReportsFragment extends Fragment {
-
-
+public class ReportsFragment extends Fragment implements View.OnClickListener {
+    private  View view;
+    private FragmentManager fm;
+    private FragmentTransaction fragmentTransaction;
+    Button moodStatistics, temperatureStatistics, periodStatistics, sexualActivityStatistics;
     public ReportsFragment() {
         // Required empty public constructor
     }
@@ -25,7 +30,35 @@ public class ReportsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reports, container, false);
+        view = inflater.inflate(R.layout.fragment_reports, container, false);
+
+        fm = getFragmentManager();
+        fragmentTransaction = fm.beginTransaction();
+
+        moodStatistics = view.findViewById(R.id.moodsStatistics);
+        sexualActivityStatistics = view.findViewById(R.id.sexualActivityStatistics);
+        temperatureStatistics = view.findViewById(R.id.temperatureStatistics);
+        periodStatistics = view.findViewById(R.id.periodStatistics);
+
+        moodStatistics.setOnClickListener(this);
+        sexualActivityStatistics.setOnClickListener(this);
+        temperatureStatistics.setOnClickListener(this);
+        moodStatistics.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.periodStatistics:
+                break;
+            case R.id.sexualActivityStatistics:
+                break;
+            case R.id.moodsStatistics:
+                break;
+            case R.id.temperatureStatistics:
+                break;
+        }
+    }
 }
---
+++ Capstone/Capstone/app/src/main/java/ca/mohawk/le/mytime/SexualActivityStatisticsFragment.java
@@ -0,0 +1,33 @@
+package ca.mohawk.le.mytime;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SexualActivityStatisticsFragment extends Fragment {
+    private View view;
+
+    public SexualActivityStatisticsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_sexual_activity_statistics, container, false);
+
+        return view;
+    }
+
+}
